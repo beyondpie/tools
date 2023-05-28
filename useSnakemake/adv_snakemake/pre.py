6f3e58d152f41ce7e5f01253bd95571880a36f28
beyondpie <panshi.pie@gmail.com>
2023-05-27 21:33:38 -0700

    minor

--- useSnakemake/adv_snakemake/pre.py
+++ useSnakemake/adv_snakemake/pre.py
@@ -1,4 +1,13 @@
 import snapatac2 as sa2
 print(sa2.__version__)
+
+a = snakemake.output
+print(type(a))
+print(a)
 print(snakemake.output[0])
+
+b = snakemake.params
+print(type(b))
+print(b)
 print(snakemake.params[0])
+
