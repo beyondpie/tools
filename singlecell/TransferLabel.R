227a28ce114abfd503fcffb14b2178210884e8bf
beyondpie <panshi.pie@gmail.com>
2024-04-20 17:06:53 -0700

    finish run debug test for transfer label with cca

--- singlecell/TransferLabel.R
+++ singlecell/TransferLabel.R
@@ -1,115 +1,97 @@
 #!/usr/bin/env Rscript
 
-library(Matrix)
+Sys.setenv("_R_USE_PIPEBIND_" = TRUE)
 options(future.globals.maxSize = 5e9)
-library(ggplot2)
-
-import::from(.from = optparse, make_option,
-  OptionParser, add_option, parse_args)
-import::from(.from = stringr, str_glue)
-import::from(.from = dplyr, group_by, slice_sample)
-import::from(.from = Seurat, VariableFeatures,
-  `VariableFeatures<-`,
-  FindVariableFeatures,
-  FindTransferAnchors,
-  TransferData, DefaultAssay)
-
-# This script is to provide a unified transfer label module
-# - Only works for two dataset at one time
-# - cca, rpca
-# - support co-embedding plot
-# - automatically label group based on votes
-# - support consensus matrix plot
-# - input format is Seurat
-
-# NOTE:
-# - R version >= 4.2
-# - Seurat version >= 5.0
+library(optparse)
+library(Matrix)
+library(Seurat)
+library(tidyverse)
 
 # * set up arguments
-args <- OptionParser() |>
-  add_option(opt_str = c("-r", "--ref"), type = "character",
-    help = "reference seurat object fnm") |>
-  add_option(opt_str = c("-q", "--query"), type = "character",
-    help = "query seurat object fnm") |>
-  add_option(opt_str = c("-o", "--outdir"), type = "character",
-    help = "the directory to save the results") |>
-  add_option(opt_str = c("--downsample"), type = "logical",
-    action = "store_true", default = FALSE) |>
-  add_option(opt_str = c("--dcolref"), type = "character",
-    help = "col of ref for downsample") |>
-  add_option(opt_str = c("--dcolquery"), type = "character",
-    help = "col of query for downsample") |>
-  add_option(opt_str = c("--ndref"), type = "integer",
-    default = NULL,
-    help = "# of cells per cluster for ref if downsampling") |>
-  add_option(opt_str = c("--ndquery"), type = "integer",
-    default = NULL,
-    help = "# of cells per cluster for query if downsampling") |>
-  add_option(opt_str = c("--nref"), type = "integer",
-    default = 200000,
-    help = "# of cells in total for ref") |>
-  add_option(opt_str = c("--nquery"), type = "integer",
-    default = 200000,
-    help = "# of cells in total for query") |>
-  add_option(opt_str = c("-f", "--feature"), type = "character",
-    default = NULL, help = "gene list file as feature") |>
-  add_option(opt_str = c("--nvar"), type = "integer",
-    default = 2000, help = " # of variable features") |>
-  add_option(opt_str = c("--useref"), type = "logical",
+args <- OptionParser(
+  usage = "Seurat Transfer Label",
+  description = "
+ NOTE:
+ - R version >= 4.2
+ - Seurat version >= 5.0
+ - Methods:cca (rpca and harmony not yet)
+
+ Usage examples
+ 0. Get help
+    Rscript TransferLabel.R -h
+"
+) |>
+  add_option(
+    opt_str = c("-r", "--ref"), type = "character",
+    help = "reference seurat object fnm"
+  ) |>
+  add_option(
+    opt_str = c("-q", "--query"), type = "character",
+    help = "query seurat object fnm"
+  ) |>
+  add_option(
+    opt_str = c("-o", "--outdir"), type = "character",
+    help = "the directory to save the results"
+  ) |>
+  add_option(
+    opt_str = c("-f", "--feature"), type = "character",
+    default = NULL, help = "gene list file as feature"
+  ) |>
+  add_option(
+    opt_str = c("--nvar"), type = "integer",
+    default = 2000, help = " # of variable features"
+  ) |>
+  add_option(
+    opt_str = c("--useref"), type = "logical",
     action = "store_ture", default = FALSE,
-    help = "use variable features from ref") |>
-  add_option(opt_str = c("--usequery"), type = "logical",
-    action = "store_true", default = FALSE) |>
-  add_option(opt_str = c("--saveanchor"), type = "logical",
-    action = "store_true", default = FALSE) |>
-  add_option(opt_str = c("-m", "--method"), type = "character",
+    help = "use variable features from ref"
+  ) |>
+  add_option(
+    opt_str = c("--usequery"), type = "logical",
+    action = "store_true", default = FALSE
+  ) |>
+  add_option(
+    opt_str = c("--saveanchor"), type = "logical",
+    action = "store_true", default = FALSE
+  ) |>
+  add_option(
+    opt_str = c("-m", "--method"), type = "character",
     default = "cca",
-    help = "method for transfer label") |>
-  add_option(opt_str = c("-k", "--kanchor"), type = "integer",
-    defautl = 5,
-    help = "kanchor for finding anchors") |>
-  add_option(opt_str = c("-p", "--npca"), type = "integer",
-    default = 50, help = "# of components for finding anchors") |>
-  add_option(opt_str = c("-t", "--tfcol"), type = "character",
-    help = "on which col of ref we transfer labels to query.") |>
-    add_option(opt_str = c("--ordcolref"), type = "character",
-      help = "ordered cols of ref for consensus matrix plot.") |>
-    add_option(opt_str = c("--ordcolquery"), type = "character",
-      help = "ordered cols of query for consensus matrix plot.") |>
-    add_option(opt_str = c("--ecref"), type = "character",
-      default = NULL,
-      help = "extra col in ref for consensus matrix plot.") |>
-    add_option(opt_str = c("--ecquery"), type = "character",
-      default = NULL,
-      help = "extra col in query for consensus matrix plot.") |>
-    add_option(opt_str = c("--ordecref"), type = "character",
-      default = NULL,
-      help = "ordered extra cols of ref.") |>
-    add_option(opt_str = c("--ordecquery"), type = "character",
-      default = NULL,
-      help = "ordered extra cols of query.") |>
-    add_option(opt_str = c("--pltcoembed"), type = "logical",
-      action = "store_true", default = FALSE,
-      help = "if plot coembedding on two datasets.") |>
+    help = "method for transfer label"
+  ) |>
+  add_option(
+    opt_str = c("-k", "--kanchor"), type = "integer",
+    default = 5,
+    help = "kanchor for finding anchors"
+  ) |>
+  add_option(
+    opt_str = c("-p", "--npca"), type = "integer",
+    default = 50, help = "# of components for finding anchors"
+  ) |>
+  add_option(
+    opt_str = c("-t", "--tfcol"), type = "character",
+    help = "on which col of ref we transfer labels to query."
+  ) |>
   parse_args(object = _)
 
 # * DEBUG
-## datadir <- file.path("~/git-recipes/mouseBrainAtlas",
-##   "amb_pairedtag", "03.integration", "src/test/resource")
-## args$ref <- file.path(datadir, "allen_ref.rds")
-## args$query <- file.path(datadir, "pt_query.rds")
-## args$tfcol <- "cl"
-## args$outdir <- datadir
-## args$downsample <- TRUE
-## args$dcolref <- "cl"
-## args$dcolquery <- "L3"
-## args$useref <- TRUE
-## args$saveanchor <- TRUE
-## args$kanchor <- 50
+projdir <- "/tscc/projects/ps-renlab2/szu/projects/amb_pairedtag"
+datadir <- file.path(
+  projdir, "03.integration", "src/test/resource"
+)
+args$ref <- file.path(datadir, "allen_ref.rds")
+args$query <- file.path(datadir, "pt_query.rds")
+args$tfcol <- "cl"
+args$outdir <- datadir
+args$useref <- TRUE
+args$saveanchor <- TRUE
+args$kanchor <- 50
+args$method <- "cca"
+args$feature <- file.path(
+  projdir, "meta", "AIT21_k8_markers.txt"
+)
 
 # * set up inputs
-eps <- 1e-6
 # pretty slow
 getVarGenes <- function(s5) {
   m <- s5@assays$RNA@layers$counts
@@ -121,25 +103,18 @@ getVarGenes <- function(s5) {
 
 getNoZeroCountGene <- function(s5) {
   m <- s5@assays$RNA@layers$counts
-  rownames(s5)[rowSums(m) > eps]
+  rownames(s5)[rowSums(m) >= 1]
 }
 
-message("Run Seurat Transfer Label.")
+message("Load ref seurat.")
 ref <- readRDS(args$ref)
 genes_ref_var <- getNoZeroCountGene(ref)
 
-if (is.null(args$tfcol)) {
-  stop("error: transfer col is NULL.")
-}
-if (!args$tfcol %in% colnames(ref@meta.data)) {
-  stop(str_glue("error: {args$tfcol} is not in ref meta data."))
+if (is.null(args$tfcol) || (!args$tfcol %in% colnames(ref@meta.data))) {
+  stop("error: tfcol is NULL or not exist in ref.")
 }
 
-if ((!is.null(args$extrpltcol)) && (
-  !args$extrpltcol %in% colnames(ref@meta.data))) {
-  stop(str_glue("error: {args$extrpltcol} is not in ref meta data."))
-}
-
+message("Load query seruat.")
 query <- readRDS(args$query)
 genes_query_var <- getNoZeroCountGene(query)
 
@@ -149,69 +124,33 @@ ref <- ref[genes_all, ] |>
   Seurat::NormalizeData(object = _)
 query <- query[genes_all, ] |>
   Seurat::NormalizeData(object = _)
-rqlist <- list(ref = ref, query = query)
-rm(ref, query)
 
-# * outdir prepare
+message("prepare outdir.")
 dir.create(args$outdir, showWarnings = FALSE)
-message("Results will be put under: ", args$outdir)
 
-# * downsampling
-
-if (args$downsample) {
-  rqlist2 <- lapply(names(rqlist), \(nm) {
-    message(str_glue("Perform downsampling on {nm}."))
-    s <- rqlist[[nm]]
-    clusters <- s@meta.data[[
-      args[[paste0("dcol", nm)]]
-    ]]
-    ntotal <- ncol(s)
-    ncl <- length(table(clusters))
-    nallow <- min(ntotal, args[[paste0("n", nm)]])
-    nd <- args[[paste0("nd", nm)]] |>
-      x => ifelse(test = is.null(x),
-        floor(nallow / ncl), x)
-    message(str_glue("In total, {ntotal} cells and {ncl} d-clusters."))
-    message(str_glue("Downsample {nd} per cluster",
-      " and {nallow} cells at most."))
-    barcodes <- data.frame(
-      barcode = colnames(s),
-      cluster = clusters) |>
-      group_by(cluster) |>
-      slice_sample(n = nd) |>
-      x => x$barcode
-    message(str_glue("{length(barcodes)} barcodes are kept."))
-    s[, barcodes]
-  })
-} else {
-  rqlist2 <- rqlist
-}
-names(rqlist2) <- names(rqlist)
-rm(rqlist)
-
-# * set up features
+message("Set up features for transfer label.")
 if (!is.null(args$feature)) {
-  message(str_glue("load features from file: {option$feature}."))
-  features <- read.table(file = args$feature, header = FALSE,
-    row.names = FALSE, col.names = FALSE)$V1
+  message(str_glue("load features from file: {args$feature}."))
+  features <- read.table(
+    file = args$feature, header = FALSE
+  )$V1
   features <- intersect(features, genes_all)
-  VariableFeatures(rqlist2$ref) <- features
-  VariableFeatures(rqlist2$query) <- features
 } else {
+  message("Use variable features.")
   nvar <- args$nvar
   message(str_glue("Finding {nvar} variable features."))
-  rqlist2$ref <- FindVariableFeatures(
-    object = rqlist2$ref,
+  ref <- FindVariableFeatures(
+    object = ref,
     selection.method = "vst",
     nfeatures = nvar
   )
-  fea_ref <- VariableFeatures(rqlist2$ref)
-  rqlist2$query <- FindVariableFeatures(
-    object = rqlist2$query,
+  fea_ref <- VariableFeatures(ref)
+  query <- FindVariableFeatures(
+    object = query,
     selection.method = "vst",
     nfeatures = nvar
   )
-  fea_query <- VariableFeatures(rqlist2$query)
+  fea_query <- VariableFeatures(query)
   if (args$useref && (!args$usequery)) {
     message("use features from REF Variable features.")
     features <- fea_ref
@@ -227,11 +166,11 @@ if (!is.null(args$feature)) {
   }
   if ((!args$useref) && (!args$usequery)) {
     message("use features from BOTH features.")
-    features <- genes_all
+    features <- union(fea_ref, fea_query)
   }
 }
-VariableFeatures(rqlist2$ref) <- features
-VariableFeatures(rqlist2$query) <- features
+VariableFeatures(ref) <- features
+VariableFeatures(query) <- features
 
 # * set up anchor process
 if (args$saveanchor) {
@@ -241,10 +180,13 @@ if (file.exists(anchorfnm)) {
   message("Anchor file exist, and will load it.")
   anchors <- readRDS(anchorfnm)
 } else {
+  if (args$method != "cca") {
+    stop(str_glue("{args$method} has been supported yet."))
+  }
   message(str_glue("FindTransferAnchors with {args$method} method."))
   anchors <- FindTransferAnchors(
-    reference = rqlist2$ref,
-    query = rqlist2$query,
+    reference = ref,
+    query = query,
     scale = TRUE,
     reduction = args$method,
     normalization.method = "LogNormalize",
@@ -262,28 +204,26 @@ if (file.exists(anchorfnm)) {
 
 # * set up transfer labels and annotations
 message("Then transfer labels in single-cell level.")
-rqlist2$query <- TransferData(
+query <- TransferData(
   anchorset = anchors,
-  refdata = rqlist2$ref@meta.data[[args$tfcol]],
+  refdata = ref@meta.data[[args$tfcol]],
   reference = NULL,
-  query = rqlist2$query,
-  weight.reduction = ifelse(
-    args$method != "cca", "pcaproject", "cca"),
+  query = query,
+  weight.reduction = args$method,
   l2.norm = FALSE,
   dims = seq_len(args$npca),
   # default
   k.weight = 50,
   verbose = TRUE,
-  )
+)
 message("Transferred labels are written into query.")
 
-queryfnm <- file.path(args$outdir,
-  str_glue("query.with.tflabels.{args$tfcol}.rds"))
-message("Save query object with tflabels to: ",
-  queryfnm)
-saveRDS(object = rqlist2$query, queryfnm)
-
-# * generate consensus matrix
-
-# * plot consensus matrix
-# * plot co-embedding
+queryfnm <- file.path(
+  args$outdir,
+  str_glue("query.with.tf-{args$method}_on-{args$tfcol}.rds")
+)
+message(
+  "Save query object with tflabels to: ",
+  queryfnm
+)
+saveRDS(object = query, queryfnm)
